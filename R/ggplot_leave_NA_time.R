e94c029381122ba8b0fe3b7e9db07dc913846576
laurin osterholt <laurin_f@posteo.de>
2022-05-10 15:15:26 +0200

    devtools::document fuzt nicht

--- R/ggplot_leave_NA_time.R
+++ R/ggplot_leave_NA_time.R
@@ -23,19 +23,24 @@ leave_NAtime_plot <- function(y="CO2",
                               plot=T,
                               adj_grob_size=T, ...,
                               geom="line"){
-
+  
   data_order <- data[order(data$date),]
   data_sub <- data_order[!is.na(data_order[,group]),]
   starts<- data_sub$date[c(1,which(difftime(data_sub$date[-1],data_sub$date[-nrow(data_sub)],units = "mins") > timestep) +1)]
   ends<- data_sub$date[c(which(difftime(data_sub$date[-1],data_sub$date[-nrow(data_sub)],units = "mins") > timestep),nrow(data_sub))]
-
+  
   period.df <- data.frame(starts,ends)
-
+  
   data_sub$period <- NA
   for(i in 1:nrow(period.df)){
     data_sub$period[data_sub$date >= period.df$starts[i] & data_sub$date <= period.df$ends[i] ] <- i
   }
-
+  
+  col_lab <- T
+  if(! col %in% colnames(data_sub)){
+    col_lab <- F
+    data_sub[,col] <- 1
+  }
   if(plot ==F){
     return(data_sub)
   }else{
@@ -48,14 +53,21 @@ leave_NAtime_plot <- function(y="CO2",
     }
     p<- p+
       facet_wrap(~period,scales="free_x",nrow=1)+
-      labs(col=col,y=ylab)+
       theme(strip.text.x = element_blank())
+    if(col_lab){
+      p<- p+
+        labs(col=col,y=ylab)
+    }else{
+      p<- p+
+        guides(col=F)+
+        labs(y=ylab)
+    }
     if(adj_grob_size == T){
       p <- adj_grob_size(p,data_sub,...)
     }
     p
     #return(p)
-    }
+  }
 }
 
 #' function to adjust the size of the facets used in \code{leave_NAtime_plot}
@@ -73,27 +85,27 @@ adj_grob_size <- function(p, data ,breaks="10 hours",date_labels = "%b %d",plot=
   # convert ggplot object to grob object
   p <- p+scale_x_datetime(date_breaks = breaks, date_labels = date_labels, ...)
   gp <- ggplotGrob(p)
-
-
+  
+  
   # get gtable columns corresponding to the facets (5 & 9, in this case)
   facet.columns <- gp$layout$l[grepl("panel", gp$layout$name)]
-
+  
   # get the number of unique x-axis values per facet (1 & 3, in this case)
-
+  
   x.var <- sapply(unique(data$period),
                   function(x) difftime(max(data$date[data$period==x]),min(data$date[data$period==x]),units="mins"))
   #oder
   #x.var <- sapply(unique(data$period),
   #                function(x) length(which(data$period==x )))
-
-
+  
+  
   # change the relative widths of the facet columns based on
   # how many unique x-axis values are in each facet
   gp$widths[facet.columns] <- gp$widths[facet.columns] * (x.var/sum(x.var))
-
+  
   # plot result
   if(plot==T){
-  grid::grid.draw(gp)
+    grid::grid.draw(gp)
   }
   return(gp)
 }
--- R/injection_arduino.R
+++ R/injection_arduino.R
@@ -1,28 +1,47 @@
+#' calculate injection rate from arduino files
+#'
+#' @param datelim
+#' @param data
+#' @param t_init
+#' @param t_min
+#' @param t_max
+#' @param plot
+#'
+#' @return
+#' @import readxl
+#' @import lubridate
+#' @import imuteTS
+#' @export
+#'
+#' @examples
+#' test
 injection_arduino <- function(datelim,
                               data = NULL,
-                              t_init = 0.5,
+                              t_init = 1,
                               t_min = 3,
-                              t_max = 5) {
-  
+                              t_max = 5,
+                              plot = "facets",
+                              return_ls = T) {
+
   ########################
   #Metadaten aus Kammer laden
-  
+
   Vol.xlsx<-readxl::read_xlsx(paste0(metapfad_tracer,"Diffusionskammer.xlsx"))
   Vol_ml<-Vol.xlsx$Volumen_effektiv_ml
-  
+
   if(is.character(datelim)){
     datelim <- ymd_hm(datelim)
   }
-  
+
   if(is.null(data)){
     files <- list.files(chamber_arduino_pfad,pattern = "_inj",full.names = F)
-    
-    
+
+
     ##subset of files with date in datelim
     file_date <- lubridate::ymd(stringr::str_extract(files,"^\\d{6}"))
     dates <- lubridate::date(datelim)
     files <- files[file_date >= dates[1] & file_date <= dates[2]]
-    
+
     if(length(files)>0){
       #read files
       data_ls <- lapply(paste0(chamber_arduino_pfad,files),read.table,sep=";",header=T,stringsAsFactors = F,fill=T)
@@ -31,15 +50,15 @@ injection_arduino <- function(datelim,
       data <- do.call(rbind,data_ls)
       data$date <- ymd_hms(data$date)
       colnames(data) <- c("date","CO2","counter","inj")
-      
+
       data <- data[order(data$date),]
-      
+
       ###
       #aufbereiten
       data$CO2 <- as.numeric(data$CO2)
       data$CO2[data$CO2 < 300| data$CO2 > 7500] <- NA
       data$CO2 <- imputeTS::na_interpolation(data$CO2,maxgap = 10)
-      
+
       data_sub <- subset(data,date > datelim[1] & date < datelim[2] & !is.na(data$date))
     }else{
       warning("no dynament data in datelim")
@@ -47,12 +66,12 @@ injection_arduino <- function(datelim,
   }else{
     data_sub <- subset(data,date > datelim[1] & date < datelim[2] & !is.na(data$date))
   }
-  
+
   probe1 <- read_sampler(datelim=datelim,format="wide")
   if(nrow(probe1) > 0){
     probe1$T_C
     data_sub <- merge(data_sub,probe1[,c("date","T_C")],all = T)
-    
+
     data_sub$T_C <- imputeTS::na_interpolation(data_sub$T_C)
     data_sub <- subset(data_sub,!is.na(inj))
   }
@@ -61,25 +80,25 @@ injection_arduino <- function(datelim,
   if(exists("data_sub")){
     closingID <- which(diff(data_sub$inj) == 1)+1
     openingID <- which(diff(data_sub$inj) == -1)
-    
+
 
     if(length(closingID) > 0 & length(openingID) > 0 ){
       if(closingID[1] > openingID[1]){
         closingID <- c(1,closingID)
       }
-      
+
       if(tail(closingID,1) > tail(openingID,1)){
         openingID <- c(openingID,nrow(data_sub))
       }
-      
+
       closing_date <- data_sub$date[closingID]
       opening_date <- data_sub$date[openingID]
-      
+
       meas_time <- as.numeric(difftime(opening_date,closing_date,unit="mins"))
-      
+
       closingID <- closingID[meas_time > t_min]
       openingID <- openingID[meas_time > t_min]
-      
+
       data_sub$zeit <- NA
       data_sub$messid <- NA
       for (i in 1:length(openingID)) {
@@ -92,18 +111,18 @@ injection_arduino <- function(datelim,
       }
       data_sub$zeit[data_sub$zeit > t_max | data_sub$zeit < 0] <- NA
       data_sub$messid[is.na(data_sub$zeit)] <- NA
-      
-      
+
+
       flux_ls <- calc_flux(data = data_sub[!is.na(data_sub$CO2),],
                   group="messid",
                   Vol=Vol_ml,
                   gas = "CO2",
                   T_deg = "T_C")
-      
+
 
       flux <- flux_ls[[1]]
       flux_data <- flux_ls[[2]]
-      
+
       if(plot == "facets"){
         names(flux_data)
         p <- ggplot(subset(flux_data,!is.na(messid)))+
@@ -112,7 +131,7 @@ injection_arduino <- function(datelim,
           #facet_wrap(~messid)+
           labs(y="CO2 (ppm) tara")+
           guides(col=F)
-        
+
         if(max(flux_data$messid,na.rm=T) > 50){
           p <- p+
             facet_wrap(~ceiling(messid / 10))
@@ -123,24 +142,22 @@ injection_arduino <- function(datelim,
         print(p)
       }
       if(plot == "timeline"){
-        p <- ggplot(flux_data)
-        p <- p+
-          geom_line(aes(date,CO2,col=as.factor(messid),group=1))+
-          labs(x="",y=expression(CO[2]))+
-          guides(col=F)
-        print(p)
+        # p <- ggplot(flux_data)
+        # p <- p+
+        #   geom_line(aes(date,CO2,col=as.factor(messid),group=1))+
+        #   labs(x="",y=expression(CO[2]))+
+        #   guides(col=F)
+
+        leave_NAtime_plot(data=flux_data,group="CO2",col="dummy")
       }
       if(plot == "flux"){
         p <- ggplot(flux)+
-          geom_line(aes(date,get(paste0(gas[1],"_mumol_per_s_m2")),col=gas[1]))+
-          labs(x="",y=expression(italic(F)~"("*mu * mol ~ m^{-2} ~ s^{-1}*")"),col="")
-        if(length(gas) > 1){
-          p <- p+geom_line(aes(date,get(paste0(gas[2],"_mumol_per_s_m2")),col=gas[2]))
-        }
+          geom_line(aes(date,CO2_ml_per_min))+
+          labs(x="",y=expression(inj~"("*ml ~ min^{-1}*")"),col="")
         print(p)
       }
-      
-      
+
+
       if(return_ls){
         return(list(flux,flux_data))
       }else{
@@ -149,3 +166,5 @@ injection_arduino <- function(datelim,
     }
   }#exists data_sub
 }
+
+
